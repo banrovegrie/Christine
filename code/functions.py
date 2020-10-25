12c094b8f6095cbebfa2011a73179cc1cb515c91
shreyas <shpradhan12@gmail.com>
2020-10-25 17:39:18 +0530

    oo lala

--- code/functions.py
+++ code/functions.py
@@ -1,13 +1,13 @@
 def tagger(attributes):
     tags = []
 
-    if attributes['TOXICITY'] >= 0.7 or attributes['SEVERE_TOXICITY'] >= 0.7:
-        tags.append('toxic')
+    if attributes['TOXICITY'] >= 0.8 and attributes['THREAT'] >= 0.8:
+        tags.append('harassment')
 
-    if attributes['INSULT'] >= 0.7 or attributes['THREAT'] >= 0.7 or attributes['IDENTITY_ATTACK'] >= 0.7:
-        tags.append('personal attack')
-
-    if attributes['FLIRTATION'] >= 0.7 or attributes['SEXUALLY_EXPLICIT'] >= 0.7 or attributes['PROFANITY'] >= 0.7:
-        tags.append('profane')
+    if attributes['IDENTITY_ATTACK'] >= 0.5 and ((attributes['FLIRTATION'] >= 0.8 and attributes['THREAT'] >= 0.8) or (attributes['SEXUALLY_EXPLICIT'] >= 0.8 and attributes['SEVERE_TOXICITY'] >= 0.8)):
+        tags.append('sexual harassment')
+    
+    if attributes['INSULT'] >= 0.9 and attributes['IDENTITY_ATTACK'] >= 0.7 and attributes['PROFANITY'] >= 0.7:
+        tags.append('inappropriate')
 
     return tags
