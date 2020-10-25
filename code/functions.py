b760803fa5ef167f9d494bd76be32e2b9888376d
banrovegrie <alapan.chaudhuri@research.iiit.ac.in>
2020-10-25 20:12:18 +0530

    updated functions.py

--- code/functions.py
+++ code/functions.py
@@ -1,15 +1,19 @@
 def tagger(attributes):
     tags = []
 
-    if attributes['TOXICITY'] >= 0.8 and attributes['THREAT'] >= 0.8:
+    # Figure out trigger constrint for harassment
+    if attributes['TOXICITY'] >= 0.8 or attributes['THREAT'] >= 0.8:
         tags.append('harassment')
 
-    if attributes['IDENTITY_ATTACK'] >= 0.5 and ((attributes['FLIRTATION'] >= 0.8 and attributes['THREAT'] >= 0.8) or (attributes['SEXUALLY_EXPLICIT'] >= 0.8 and attributes['SEVERE_TOXICITY'] >= 0.8)):
+    if attributes['IDENTITY_ATTACK'] >= 0.8:
+        tags.append('personal attack')
+
+    # Figure out trigger constraint for sexual harassment
+    if attributes['IDENTITY_ATTACK'] >= 0.4 and ((attributes['FLIRTATION'] >= 0.8 and attributes['THREAT'] >= 0.6) or (attributes['SEXUALLY_EXPLICIT'] >= 0.8 and attributes['TOXICITY'] >= 0.65)):
         tags.append('sexual harassment')
-    
-    if (attributes['INSULT'] >= 0.85 and attributes['PROFANITY'] >= 0.8) or (attributes['TOXICITY'] >= 0.9 and attributes['PROFANITY'] >= 0.8):
+
+    # Figure out trigger constraint for
+    if (attributes['INSULT'] >= 0.7 and attributes['PROFANITY'] >= 0.8) or (attributes['TOXICITY'] >= 0.7 and attributes['PROFANITY'] >= 0.8):
         tags.append('inappropriate')
 
     return tags
-
-    
